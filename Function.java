7b0438035f7337455ca3b2973de2afa8d2fdfbf1
Evelyn Shen <32460422+EvelynShene@users.noreply.github.com>
2018-06-06 11:57:38 -0400

    Update Function.java

--- Function.java
+++ Function.java
@@ -7,7 +7,7 @@ public class Varargs {
  
     /** 传递同类型的可变参数(长度不定参数)给一个方法
      * Note: 一个方法中只能指定一个可变参数，它必须是方法的最后一个参数。任何普通的参数必须在它之前声明。
-     *／
+     */
     public static void printMax( double... numbers) {
         if (numbers.length == 0) {
             System.out.println("No argument passed");
